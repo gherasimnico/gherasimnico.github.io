8efeea1e0c207eb3dabc70d4d31df814a498d558
Nico <nicoletagherasim@yahoo.com>
2024-03-04 18:21:05 +0200

    first CV using html

---
+++ examples.js
@@ -0,0 +1,65 @@
+function variablesExample() {
+  console.log(true);
+  console.info("page loaded");
+  console.debug(5 - 2);
+  console.warn(10 / 2);
+  console.error("hello" + "there");
+
+  var employed = false;
+  var age = 31 + 2;
+  var name = "Nick";
+  console.info("I'm employed", employed);
+  console.info(typeof employed);
+  console.warn("age", age, typeof age);
+  console.info("My name is", name, typeof name);
+
+  age = 35;
+  console.warn("age", age, typeof age);
+
+  age = false;
+  console.warn("age", age, typeof age);
+
+  var skills = ["HTML", "CSS", "JS"];
+  console.debug(skills, typeof skills);
+
+  var person = {
+    employed: true,
+    age: 29,
+  };
+  console.info(person, person.age, typeof person);
+}
+
+function updateTitle(title) {
+  var jobTitle = document.getElementById("job-title");
+  console.warn(jobTitle, typeof jobTitle);
+
+  console.info(jobTitle.innerHTML);
+  jobTitle.innerHTML = title;
+  console.info(jobTitle.innerHTML);
+}
+function jsonWithFunctions() {
+  console.log(typeof variablesExample);
+  console.log(typeof document);
+  console.log(typeof document.getElementById);
+
+  var person = {
+    age: 29,
+    name: "Nick",
+    learn: function () {
+      console.info("I'm learning JS, I love it!");
+    },
+    play: function () {
+      console.info("I'm playing. My name is ", this.name);
+    },
+  };
+  person.learn();
+  person.play();
+  var action = "learn"; //learn
+  person[action]();
+}
+
+//variablesExample();
+
+updateTitle("ABCD");
+
+//jsonWithFunctions();
---
+++ index.js
@@ -0,0 +1,35 @@
+function showHomePage() {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showSkillsPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("skills").style.display = "block";
+}
+function showProjectsPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+}
+function showLanguagesPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "block";
+}
+showHomePage();
+
+var homeLink = document.querySelectorAll("#top-menu-bar a")[0];
+homeLink.addEventListener("click", showHomePage);
+
+var projectsLink = document.querySelectorAll("#top-menu-bar a")[2];
+projectsLink.addEventListener("click", showProjectsPage);
+
+var languagesLink = document.querySelectorAll("#top-menu-bar a")[3];
+languagesLink.addEventListener("click", showLanguagesPage);
