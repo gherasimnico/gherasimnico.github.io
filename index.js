4f1d89390ad521013760f51daee3ad74b628c1cb
Nico <nicoletagherasim@yahoo.com>
2025-05-06 18:34:01 +0300

    image changed + few small details

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-let activePage = "skills";
+window.onerror = function (message, source, lineno, colno, error) {
+  alert(
+    `Error: ${message}\nSource: ${source}\nLine: ${lineno}\nColumn: ${colno}\nError object: ${JSON.stringify(
+      error
+    )}`
+  );
+  return false; // Returning false will suppress the default browser error message
+};
 
-function $(selector) {
-  return document.querySelector(selector);
+var activePage = "home";
+
+var dark = localStorage.getItem("dark-mode");
+if (dark === "true") {
+  document.body.classList.add("dark-mode");
+}
+
+if (typeof URLSearchParams === "undefined") {
+  var script = document.createElement("script");
+  script.src =
+    "https://cdn.jsdelivr.net/npm/url-search-params-polyfill@8.2.5/index.min.js";
+  script.async = false;
+  document.head.appendChild(script);
+}
+if (typeof fetch === "undefined") {
+  var script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/npm/fetch-polyfill@0.8.2/fetch.min.js";
+  script.async = false;
+  document.head.appendChild(script);
+}
+
+function $(selector, parent) {
+  return (parent || document).querySelector(selector);
 }
 
 function hide(id) {
@@ -53,6 +81,16 @@ function sortSkillsByEndorcements(skills) {
 
 function sortSkillsByName(skills) {
   return skills.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+function initMenu() {
+  $("#toggle-dark-mode").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+    localStorage.setItem(
+      "dark-mode",
+      document.body.classList.contains("dark-mode")
+    );
+  });
 }
 
 console.info(typeof []);
